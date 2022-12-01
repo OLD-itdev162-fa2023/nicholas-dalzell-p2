c8d45b3834a32db1921112fa4cc489ad0728ff5a
Nick Dalzell <dalzelln@gmatc.matc.edu>
2022-12-01 13:41:19 -0600

    added create and update employee http requests

--- API/Controllers/EmployeeListController.cs
+++ API/Controllers/EmployeeListController.cs
@@ -18,12 +18,85 @@ namespace API.Controllers
         {
             this.context = context;
         }
-
-        //GET api/posts
+        /// <summary>
+        /// GET api/EmployeeList
+        /// </summary>
+        /// <returns> A list of employees</returns>
         [HttpGet(Name = "GetEmployeeList")]
         public ActionResult<List<AddEmployee>> Get()
         {
             return this.context.EmployeeList.ToList();
+        }
+
+        /// <summary>
+        /// GET api/addEmployee/[id]
+        /// </summary>
+        /// <param name="id">AddEmployee id</param>
+        /// <returns> A single employee </returns>
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<AddEmployee> GetById(Guid id)
+        {
+            var AddEmployee = this.context.EmployeeList.Find(id);
+            if(AddEmployee is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(AddEmployee);
+        }
+
+        /// <summary>
+        /// POST api/AddEmployee
+        /// </summary>
+        /// <param name="request">JSON request containing employee fields</param>
+        /// <returns> A new employee</returns>
+        [HttpPost(Name = "Create")]
+        public ActionResult<AddEmployee> Create([FromBody]AddEmployee request)
+        {
+            var AddEmployee = new AddEmployee{
+                Id = request.Id,
+                Name = request.Name,
+                Wage = request.Wage,
+                HireDate = request.HireDate
+            };
+
+            context.EmployeeList.Add(AddEmployee);
+            var success = context.SaveChanges() > 0;
+
+            if(success)
+            {
+                return Ok(AddEmployee);
+            }
+
+            throw new Exception("Error creating employee");
+        }
+
+        /// <summary>
+        /// PUT api/AddEmployee
+        /// </summary>
+        /// <param name="request">JSON request containing one or more updated post fields</param>
+        /// <returns>An updated employee</returns>
+        [HttpPut(Name = "Update")]
+        public ActionResult<AddEmployee> Update([FromBody]AddEmployee request)
+        {
+            var AddEmployee = context.EmployeeList.Find(request.Id);
+            if (AddEmployee == null) 
+            {
+                throw new Exception("Could not find employee");
+            }
+            //update employee properties with request values if present
+            AddEmployee.Name = request.Name != null ? request.Name : AddEmployee.Name;
+            AddEmployee.Wage = request.Wage != 0 ? request.Wage : AddEmployee.Wage;
+            AddEmployee.HireDate = request.HireDate != null ? request.HireDate : AddEmployee.HireDate;
+
+            var success = context.SaveChanges() > 0;
+
+            if(success)
+            {
+                return Ok(AddEmployee);
+            }
+
+            throw new Exception("Error updating employee");
         }
     }
 }
